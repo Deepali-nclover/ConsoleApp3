5eae6d8bdfabcf1c925c2f1a63140e4fe096914a
deepali <deepalinavgire7350@gmail.com>
2023-08-23 16:15:25 +0530

    Edited Console msg

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,7 +10,8 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello all");
+            Console.WriteLine("Hello everyone");
+
         }
     }
 }
