8000941523384392b90ed20a49aa2f31573a73b5
DN <142329844+DeepaliNavgire@users.noreply.github.com>
2023-08-23 16:38:02 +0530

    updated for loop
    chnaged number of iteration

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-           for(int i=1;i<=10;i++)
+           for(int i=1;i<=10000;i++)
            {
                Console.WriteLine("Hello");
            }
